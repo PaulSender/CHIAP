f833694870a86d5a403c269ddde55581519a0a37
Kylemcd13 <44272896+Kylemcd13@users.noreply.github.com>
2018-10-29 17:09:03 -0400

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,37 +1,53 @@
+package client;
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Client {
-	public static void main(String[] args) {
+	
+
+	ClientConnection cc;
+	
+	
+	public Client() {
 		try {
-			Socket sock = new Socket("127.0.0.1", 1201);
-			//Once the connection is established the client tells it self that the server is connected
-			System.out.println("Server: This is Server");
-			DataInputStream din = new DataInputStream(sock.getInputStream());
-			DataOutputStream dout = new DataOutputStream(sock.getOutputStream());
-
-			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-			String msgin ="";
-			String msgout ="";
-			//Block of code responsible for reading and writing messages
-			while(!msgin.equals("end")) {
-				msgout = reader.readLine();
-				//Puts condition in a string so its not cap sensative
-				String con = "END";
-				//Once msgout is defined we can check is it meets the condition
-				if (msgout.equals(con.toLowerCase())) {
-					//Closes both client and server. The server throws an expection
-					sock.close();
-					//Ends the session
-				}
-				dout.writeUTF("Client: " + msgout);
-				msgin = din.readUTF();
-				System.out.println(msgin);
-			}
-
-			//End message block
-		} catch (Exception e) {
+		    Socket s = new Socket("localhost", 3333);
+		    cc = new ClientConnection(s, this);
+		    
+			
+			listenForInput();
+		}
+		catch(UnknownHostException e){
+			e.printStackTrace();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
 		}
 	}
+
+	public void listenForInput() {
+		Scanner console = new Scanner(System.in);
+		
+		while(true) {
+			while(!console.hasNextLine()) {
+				try {
+					Thread.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			String input = console.nextLine();
+			
+			if(input.toLowerCase().equals("quit")) {
+				break;
+			}
+			cc.sendStringToServer(input);
+		}
+		cc.close();
+	}
+	public static void main(String[] args) {
+		new Client();
+
+	}
+
 }
--- Server.java
+++ Server.java
@@ -1,36 +1,34 @@
+package server;
+
 import java.io.*;
+import java.util.*;
 import java.net.*;
+
 public class Server {
-	public static void main(String[] args) {
+	
+	ArrayList<ServerConnection> connections = new ArrayList<ServerConnection>();
+	ServerSocket ss;
+	boolean shouldRun = true;
+	
+	
+	public Server() {
 		try {
-			//Server needs to create serverSocket and socket.
-			ServerSocket sSock = new ServerSocket(1201);
-			Socket sock = sSock.accept();
-
-			DataInputStream din = new DataInputStream(sock.getInputStream());
-			DataOutputStream dout = new DataOutputStream(sock.getOutputStream());
-
-			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-			String msgin = "";
-			String msgout ="";
-			//Block of code that is responsible for reading and writing messages
-			//This while loop will run forever or until this socket is closed
-			while(!msgin.equals("end")) {
-				msgin = din.readUTF();
-				System.out.println(msgin);
-				msgout = reader.readLine();
-				dout.writeUTF("Server: " + msgout);
-				dout.flush();
+			ss = new ServerSocket(3333);
+		while(shouldRun) {
+			Socket s = ss.accept();
+			ServerConnection sc = new ServerConnection(s, this);
+			sc.start();
+			connections.add(sc);
 			}
-			//End message block
-		} catch (Exception e) {
-			//When client closes its socket, server's gets closed too so an expection gets thrown
-			/*This is where we output the session end message because we can give a message to the
-			user some information of why it's throwing an expection. We chose to give the user
-			a simple message telling them the session has ended. */
-			System.out.println("The Session has ended");
 		}
-		
+		catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
+
+	public static void main(String[] args) {
+		new Server();
+
+	}
+
 }
